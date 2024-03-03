5a1f9f792235c6ba7aba820bfa4794079094d77e
janjiraadev <45095722+janjiraadev@users.noreply.github.com>
2024-03-03 18:03:49 +0700

    mobile

--- script_data.js
+++ script_data.js
@@ -7,44 +7,44 @@ var rawData = [
     location: "Europe, North Africa, and southwestern Asia",
     habitat: "Grasslands, meadows, woodlands",
     jumprange: "Up to 2 meters",
-    runningspeed: "Up to 2 meters", 
-    food: 
-    [
-      {
-        id: 1,
-        title: "Grasses",
-        img: "images/1_EU/F-1.png"
-      },
-      {
-        id: 2,
-        title: "Berries",
-        img: "images/1_EU/F-2.png"
-      },
-      {
-        id: 3,
-        title: "Herbs",
-        img: "images/1_EU/F-3.png"
-      },
-    ],
-    imgmain: 
-    [
-      {
-        id: 1, 
-        img: "images/1_EU/01_PA.png"
-      },
-      {
-        id: 2, 
-        img: "images/1_EU/02_PA.png"
-      },
-      {
-        id: 3, 
-        img: "images/1_EU/03_PA.png"
-      },
-      {
-        id: 4, 
-        img: "images/1_EU/04_PA.png"
-      },
-    ],
+    runningspeed: "Up to 2 meters",
+    food:
+      [
+        {
+          id: 1,
+          title: "Grasses",
+          img: "images/1_EU/F-1.png"
+        },
+        {
+          id: 2,
+          title: "Berries",
+          img: "images/1_EU/F-2.png"
+        },
+        {
+          id: 3,
+          title: "Herbs",
+          img: "images/1_EU/F-3.png"
+        },
+      ],
+    imgmain:
+      [
+        {
+          id: 1,
+          img: "images/1_EU/01_PA.png"
+        },
+        {
+          id: 2,
+          img: "images/1_EU/02_PA.png"
+        },
+        {
+          id: 3,
+          img: "images/1_EU/03_PA.png"
+        },
+        {
+          id: 4,
+          img: "images/1_EU/04_PA.png"
+        },
+      ],
   },
   {
     id: "02",
@@ -53,43 +53,43 @@ var rawData = [
     location: "Eastern North America",
     habitat: "Forest edges, grasslands, meadows",
     jumprange: "Up to 1 meter",
-    runningspeed: "24 km/h", 
-    food: 
-    [
-      {
-        id: 1,
-        title: "Vegetables",
-        img: "images/2_EC/F-1.png"
-      },
-      {
-        id: 2,
-        title: "Grasses",
-        img: "images/2_EC/F-4.png"
-      },
-      {
-        id: 3,
-        title: "Herbs",
-        img: "images/2_EC/F-3.png"
-      },
-    ],
-    imgmain: 
-    [
-      {
-        id: 1, 
-        img: "images/2_EC/01_PA.png"
-      },
-      {
-        id: 2, 
-        img: "images/2_EC/02_PA.png"
-      },
-      {
-        id: 3, 
-        img: "images/2_EC/03_PA.png"
-      },
-      {
-        id: 4, 
-        img: "images/1_EU/04_PA.png"
-      },
-    ],
+    runningspeed: "24 km/h",
+    food:
+      [
+        {
+          id: 1,
+          title: "Vegetables",
+          img: "images/2_EC/F-1.png"
+        },
+        {
+          id: 2,
+          title: "Grasses",
+          img: "images/2_EC/F-4.png"
+        },
+        {
+          id: 3,
+          title: "Herbs",
+          img: "images/2_EC/F-3.png"
+        },
+      ],
+    imgmain:
+      [
+        {
+          id: 1,
+          img: "images/2_EC/01_PA.png"
+        },
+        {
+          id: 2,
+          img: "images/2_EC/02_PA.png"
+        },
+        {
+          id: 3,
+          img: "images/2_EC/03_PA.png"
+        },
+        {
+          id: 4,
+          img: "images/1_EU/04_PA.png"
+        },
+      ],
   },
 ];
