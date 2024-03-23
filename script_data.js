e2769f96590d4dc34edc862a1f272abad0ca058c
janjiraadev <45095722+janjiraadev@users.noreply.github.com>
2024-03-23 21:55:22 +0700

    information

--- script_data.js
+++ script_data.js
@@ -49,7 +49,7 @@ var rawData = [
   {
     id: "02",
     name: "Eastern Cottontail",
-    description: "hey use a technique called run and leap to confuse predators.",
+    description: "They use a technique called run and leap to confuse predators.",
     location: "Eastern North America",
     habitat: "Forest edges, grasslands, meadows",
     jumprange: "Up to 1 meter",
@@ -89,6 +89,151 @@ var rawData = [
         {
           id: 4,
           img: "images/1_EU/04_PA.png"
+        },
+      ],
+  },
+  {
+    id: "03",
+    name: "Snowshoe Hare",
+    description: "Their fur changes color from brown in summer to white in winter for camouflage.",
+    location: "Northern North America, including Alaska and Canada",
+    habitat: "Boreal forests, taiga, tundra",
+    jumprange: " Up to 3 meters",
+    runningspeed: "45 km/h",
+    food:
+      [
+        {
+          id: 1,
+          title: "Berries",
+          img: "images/3_SH/F-1.png"
+        },
+        {
+          id: 2,
+          title: "Twigs",
+          img: "images/3_SH/F-2.png"
+        },
+        {
+          id: 3,
+          title: "Grasses",
+          img: "images/3_SH/F-3.png"
+        },
+      ],
+    imgmain:
+      [
+        {
+          id: 1,
+          img: "images/3_SH/01_PA.png"
+        },
+        {
+          id: 2,
+          img: "images/3_SH/02_PA.png"
+        },
+        {
+          id: 3,
+          img: "images/3_SH/03_PA.png"
+        },
+        {
+          id: 4,
+          img: "images/3_SH/04_PA.png"
+        },
+      ],
+  },
+  {
+    id: "04",
+    name: "Pygmy Rabbit",
+    description: "They are the smallest rabbit species in North America.",
+    location: "Western United States",
+    habitat: "Sagebrush habitats, grasslands",
+    jumprange: "Up to 1 meter",
+    runningspeed: "24 km/h",
+    food:
+      [
+        {
+          id: 1,
+          title: "Sagebrush",
+          img: "images/4_PR/F-1.png"
+        },
+        {
+          id: 2,
+          title: "Grasses",
+          img: "images/4_PR/F-2.png"
+        },
+        {
+          id: 3,
+          title: "Twigs",
+          img: "images/4_PR/F-3.png"
+        },
+       
+      ],
+    imgmain:
+      [
+        {
+          id: 1,
+          img: "images/4_PR/01_PA.png"
+        },
+        {
+          id: 2,
+          img: "images/4_PR/02_PA.png"
+        },
+        {
+          id: 3,
+          img: "images/4_PR/03_PA.png"
+        },
+        {
+          id: 4,
+          img: "images/4_PR/04_PA.png"
+        },
+      ],
+  },
+
+  {
+    id: "05",
+    name: "Amami Rabbit",
+    description: "They are an endangered species with a population of fewer than 500 individuals.",
+    location: "Amami Islands, Japan",
+    habitat: "Forests, grasslands",
+    jumprange: "Up to 1 meter",
+    runningspeed: "No Information",
+    food:
+      [
+        {
+          id: 1,
+          title: "Leaves",
+          img: "images/5_AR/F-1.png"
+        },
+        {
+          id: 2,
+          title: "Grasses",
+          img: "images/5_AR/F-2.png"
+        },
+        {
+          id: 3,
+          title: "Bark",
+          img: "images/5_AR/F-3.png"
+        },
+       
+      ],
+    imgmain:
+      [
+        {
+          id: 1,
+          img: "images/5_AR/01_PA.png"
+        },
+        {
+          id: 2,
+          img: "images/5_AR/02_PA.png"
+        },
+        {
+          id: 3,
+          img: "images/5_AR/03_PA.png"
+        },
+        {
+          id: 4,
+          img: "images/5_AR/04_PA.png"
+        },
+        {
+          id: 5,
+          img: "images/5_AR/05_PA.png"
         },
       ],
   },
